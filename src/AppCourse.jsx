873ab12688b683c266795365113f8abd9b208b7c
Seongmin Bae <sm4046@naver.com>
2025-01-31 23:38:27 +0900

    Immer 라이브러리 사용해서 state 업데이트하기

--- src/AppCourse.jsx
+++ src/AppCourse.jsx
@@ -2,10 +2,11 @@ import './AppCourse.css';
 import CourseForm from './components/CourseForm.jsx';
 import Card from './components/Card.jsx';
 import { useState } from 'react';
+import { useImmer } from 'use-immer';
 
 function App() {
   // state 적용 및 좋아요 클릭 시 해당 상태 변경. 잘 작동하면 useState를  useImmer로 변경하기.
-  const [items, setCourseItems] = useState([
+  const [items, updateItems] = useImmer([
     {
       id: 0,
       title: '입문자를 위한, HTML&CSS 웹 개발 입문',
@@ -32,17 +33,21 @@ function App() {
     }
   ]);
   const handleFavoriteChange = (id, isFavorite) => {
-    const newItems = items.map((item => {
-      if (item.id === id) {
-        return {
-          ...item,
-          isFavorite
-        }
-      }
-      return item;
-    }))
+    updateItems((draft) => { // draft는 state의 복사본. Immer 라이브러리는 자동으로 배열 혹은 객체인 state를 만들어주고, 이를 직접 수정하면 내부적으로 새로운 state를 생성해준다. 
+      const targetItem = draft.find(item => item.id === id); // 객체의 참조값(주소값)을 targetItem에 할당함. state와 복사본인 draft의 내부 요소들은 동일한 주소값을 참조한다. 
+      targetItem.isFavorite = isFavorite;
+    })
+    // const newItems = items.map((item => {
+    //   if (item.id === id) {
+    //     return {
+    //       ...item,
+    //       isFavorite
+    //     }
+    //   }
+    //   return item;
+    // }))
 
-    setCourseItems(newItems);
+    // setCourseItems(newItems);
   }
 
   const favoriteList = items.filter(item => item.isFavorite); // 콜백 함수에서 true인 해당 요소를 배열에 넣음.
